bf4463343dc3bbe6d59458c35e4800368e6be362
amirMahfoozi <am.m8313@yahoo.com>
2023-02-04 14:24:29 +0330

    added file name

--- vim-phase2/main.c
+++ vim-phase2/main.c
@@ -15,12 +15,20 @@
 
 int MODE; //1 == insert // 2 ==normal // 3 == visual
 char text_box[100000] = {};
+char name[50] = "new text file";
 void clear_text_box()
 {
 	for(int i = 0;i<100000;i++)
 	{
 		text_box[i] = 0;
 	}
+}
+void clear_name()
+{
+        for(int i = 0;i<50;i++)
+        {
+                name[i] = 0;
+        }
 }
 int navigation(WINDOW* win,char temp,int *index)
 {
@@ -51,6 +59,51 @@ int navigation(WINDOW* win,char temp,int *index)
                 return 1;
         }
 }
+
+void open(WINDOW *win,WINDOW *win_mode,char address[])
+{
+    wclear(win_mode);
+    box(win_mode,0,0);
+    wattron(win_mode,COLOR_PAIR(1));
+        wattron(win_mode,A_BOLD);
+
+    mvwprintw(win_mode,1,1,"OPENED %s",name);
+    wrefresh(win_mode);
+    wattroff(win_mode,COLOR_PAIR(1));
+        wattroff(win_mode,A_BOLD);
+
+    int n = strlen(address);
+    char filename[max_address];
+    for(int i = 1;i<n;i++)
+    {
+        filename[i-1] = address[i];
+    }
+    filename[n-1] = '\0';
+    // filename = address - '/'
+    FILE* myfile;
+    if(fopen(filename,"r")) // check if file is created
+    {
+        myfile = fopen(filename,"r");
+        char line[200];
+	int cnt_line = 1;
+	int ymax,xmax;
+	getmaxyx(win,ymax,xmax);
+        while(1)
+        {
+		if(cnt_line == ymax-1) break;
+            line[0] = '\0';
+            if(fgets(line,200,myfile) == NULL) break;
+            mvwprintw(win,cnt_line,1,"%s",line);
+	    cnt_line++;
+        }
+        fclose(myfile);
+    }
+    else{
+        wprintw(win,"This file doesn't exist !\n");
+        return;
+    }
+}
+
 void copy_for_undo(char address[])
 {
     int n = strlen(address);
@@ -269,9 +322,9 @@ void normal_mode()
 	echo();
 	start_color();
         init_pair(1,COLOR_BLUE,COLOR_WHITE);
-        int y_max = 20,x_max = 100;
+	int y_max = 20,x_max = 100;
         getmaxyx(stdscr,y_max,x_max);
-	//create a window for input
+        //create a window for input
         WINDOW *input = newwin(3,x_max,y_max-3,0);
         box(input,0,0);
         refresh();
@@ -281,11 +334,12 @@ void normal_mode()
         box(mode_win,0,0);
         refresh();
         wrefresh(mode_win);
-	//create a window for text
+        //create a window for text
         WINDOW *text = newwin(y_max-6,x_max,0,0);
         box(text,0,0);
         refresh();
         wrefresh(text);
+
 	MODE = 2;
         wattron(mode_win,COLOR_PAIR(1));
         wattron(mode_win,A_BOLD);
@@ -314,6 +368,38 @@ void normal_mode()
 		mvwscanw(input,1,1,"%[^\n]%*c",filename);
 		createfile(filename);
 		copy_from_new_to_file(filename);
+		wclear(mode_win);
+		box(mode_win,0,0);
+		wattron(mode_win,COLOR_PAIR(2));
+        wattron(mode_win,A_BOLD);
+		mvwprintw(mode_win,1,1,"SAVED");
+		wrefresh(mode_win);
+        wattroff(mode_win,A_BOLD);
+        wattroff(mode_win,COLOR_PAIR(2));
+
+		wrefresh(mode_win);
+	}
+	else if(!strcmp(command,"open"))
+	{
+		wclear(input);
+                wclear(mode_win);
+                box(mode_win,0,0);
+                init_pair(2,COLOR_MAGENTA,COLOR_BLACK);
+                wattron(mode_win,COLOR_PAIR(2));
+        wattron(mode_win,A_BOLD);
+        mvwprintw(mode_win,1,1,"ENTER THE NAME :");
+        wrefresh(mode_win);
+        wattroff(mode_win,A_BOLD);
+        wattroff(mode_win,COLOR_PAIR(2));
+
+                box(input,0,0);
+                wrefresh(input);
+                mvwscanw(input,1,1,"%[^\n]%*c",filename);
+		clear_name();
+		strcpy(name,filename);
+		open(text,mode_win,filename);
+		wrefresh(text);
+
 	}
 	wclear(input);
 	box(input,0,0);
@@ -322,11 +408,12 @@ void normal_mode()
 }
 void insert_mode()
 {
+	clear_text_box();
 	echo();
 	start_color();
 	init_pair(1,COLOR_BLUE,COLOR_WHITE);
         init_pair(2,COLOR_MAGENTA,COLOR_BLACK);
-        int y_max = 20,x_max = 100;
+	int y_max = 20,x_max = 100;
         getmaxyx(stdscr,y_max,x_max);
         //create a window for input
         WINDOW *input = newwin(3,x_max,y_max-3,0);
@@ -338,15 +425,17 @@ void insert_mode()
         box(mode_win,0,0);
         refresh();
         wrefresh(mode_win);
-	//create a window for text
-	WINDOW *text = newwin(y_max-6,x_max,0,0);
-	box(text,0,0);
-	refresh();
-	wrefresh(text);
+        //create a window for text
+        WINDOW *text = newwin(y_max-6,x_max,0,0);
+        box(text,0,0);
+        refresh();
+        wrefresh(text);
+
 	 MODE = 1;
         wattron(mode_win,COLOR_PAIR(1));
         wattron(mode_win,A_BOLD);
-        mvwprintw(mode_win,1,1,"INSERT");
+        mvwprintw(mode_win,1,1,"INSERT   ");
+	wprintw(mode_win,"%s +",name);
         wrefresh(mode_win);
         wattroff(mode_win,A_BOLD);
         wattroff(mode_win,COLOR_PAIR(1));
@@ -369,9 +458,9 @@ void insert_mode()
 		{
 			pos++;
 		}
-
+		
 		ch = mvwgetch(text,line,pos);
-
+		
 	}
 	text_box[index] = 0;
 	FILE *newfile = fopen("main_page.txt","w");
@@ -388,25 +477,30 @@ int main()
 {
 	initscr();
 	cbreak();
+	int y_max = 20,x_max = 100;
+        getmaxyx(stdscr,y_max,x_max);
+        //create a window for input
+        WINDOW *input = newwin(3,x_max,y_max-3,0);
+        box(input,0,0);
+        refresh();
+        wrefresh(input);
+        //create a window for mode
+        WINDOW *mode_win = newwin(3,x_max,y_max-6,0);
+        box(mode_win,0,0);
+        refresh();
+        wrefresh(mode_win);
+        //create a window for text
+        WINDOW *text = newwin(y_max-6,x_max,0,0);
+        box(text,0,0);
+        refresh();
+        wrefresh(text);
+
         while(1)
 	{
 		char temp;
 		noecho();
 		start_color();
         	init_pair(1,COLOR_BLUE,COLOR_WHITE);
-   	        int y_max = 20,x_max = 100;
-  	        getmaxyx(stdscr,y_max,x_max);
-       		//create a window for input
-       	        WINDOW *input = newwin(3,x_max,y_max-3,0);
-   	        box(input,0,0);
-       	        refresh();
- 	        wrefresh(input);
-	        //create a window for mode
-   	        WINDOW *mode_win = newwin(3,x_max,y_max-6,0);
-    	        box(mode_win,0,0);
-	        refresh();
-        	wrefresh(mode_win);
-
 		temp = getch();
 		if(temp == 'e')
 		{
@@ -418,13 +512,13 @@ int main()
 		}
 		else if(temp == 'v')
 		{
-
+		 
 		}
 		else if(temp == ':')
 		{
 		  normal_mode();
 		}
-	}
+	}	
 	endwin();
 	return 0;
 }
